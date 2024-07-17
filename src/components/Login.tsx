ad54f46772bff5fe78281eabb2e4db7c9c82cdd2
Owen Alistair Steel <woofdog04@gmail.com>
2024-07-17 14:06:08 +0100

    Creates basic login frontend for Google OAuth

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,22 +2,27 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { GoogleOAuthProvider, GoogleLogin, CredentialResponse } from '@react-oauth/google';
 import Cookies from 'js-cookie';
-import { login } from '../store/userSlice'; // Adjust the path as needed
+import { login } from '../store/userSlice';
 import { useNavigate } from 'react-router-dom';
-
-import "./Login.css"
+import axiosAPI from '../services/api';
 
 const Login: React.FC = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const handleLoginSuccess = (response: CredentialResponse) => {
-        // Insert backend OAuth logic...
-        if (response.credential) {
-            // Set a cookie to keep the user logged in
-            Cookies.set('auth_token', response.credential, { expires: 7 }); // Expires in 7 days
-            dispatch(login());
-            navigate('/dashboard');
+    const handleLoginSuccess = async (credentialResponse: CredentialResponse) => {
+        if (credentialResponse.credential) {
+            try {
+                const response = await axiosAPI.post('/auth/google', { token: credentialResponse.credential });
+                const { authToken } = response.data;
+
+                // Set a cookie to keep the user logged in
+                Cookies.set('auth_token', authToken, { expires: 7 }); // Expires in 7 days
+                dispatch(login());
+                navigate('/dashboard');
+            } catch (error) {
+                console.error('Login failed:', error);
+            }
         }
     };
 
@@ -28,7 +33,6 @@ const Login: React.FC = () => {
     return (
         <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || ''}>
             <div>
-                <h2>Login</h2>
                 <GoogleLogin
                     onSuccess={handleLoginSuccess}
                     onError={handleLoginFailure}
