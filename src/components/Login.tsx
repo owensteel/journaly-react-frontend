760491abd50aa8392ea1364de06200d12c1b2eb4
Owen Alistair Steel <woofdog04@gmail.com>
2024-07-21 19:35:36 +0100

    Adds user-visible error handling to Login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,10 +5,18 @@ import Cookies from 'js-cookie';
 import { login } from '../store/userSlice';
 import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../services/api';
+import { useAlert } from './AlertContext';
 
 const Login: React.FC = () => {
+    const { showAlert } = useAlert();
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    const handleLoginFailure = () => {
+        console.error('Login failed');
+        showAlert('Something went wrong. Please try again.', 'error', 'Error');
+    };
 
     const handleLoginSuccess = async (credentialResponse: CredentialResponse) => {
         if (credentialResponse.credential) {
@@ -22,13 +30,10 @@ const Login: React.FC = () => {
                 dispatch(login({ name: user.name, picture: user.picture }));
                 navigate('/dashboard');
             } catch (error) {
-                console.error('Login failed:', error);
+                console.error(error)
+                handleLoginFailure()
             }
         }
-    };
-
-    const handleLoginFailure = () => {
-        console.error('Login failed');
     };
 
     return (
