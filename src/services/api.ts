07efaedb0037ca13c2668e14c992cd4af979e901
Owen Alistair Steel <woofdog04@gmail.com>
2024-07-22 19:42:34 +0100

    Fixes typo on API baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: window.location.hostname == "localhost" ? "http://localhost:3001" : "http://191.101.81.26:3001/",
+    baseURL: window.location.hostname == "localhost" ? "http://localhost:3001" : "http://191.101.81.26:3001",
 });
 
 export default api;
